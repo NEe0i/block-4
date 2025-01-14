8ff1682ded3824f547fb47b962fea29dec820b70
NEe0i <vadimnikolaev2003145@gmail.com>
2025-01-14 15:07:46 +0300

    Add files via upload

---
+++ task21.py
@@ -0,0 +1,13 @@
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+
+def is_divisor(a, b):
+    if b % a == 0:
+        return True
+    else:
+        return False
+
+if is_divisor(a, b):
+    print(f"{a} является делителем {b}.")
+else:
+    print(f"{a} не является делителем {b}.")

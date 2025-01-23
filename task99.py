8438e81b44a1cd5e9f5e285fb12dc628779a2089
NEe0i <vadimnikolaev2003145@gmail.com>
2025-01-23 11:11:19 +0000

    Вадим

---
+++ task99.py
@@ -0,0 +1,10 @@
+number1 = int(input("Введите число: "))
+number2 = int(input("Введите число: "))
+number3 = int(input("Введите число: "))
+
+if number1 <= number2 and number1 <= number3:
+    print(number2 + number3)
+elif number2 <= number1 and number2 <= number3:
+    print(number1 + number3)
+else:
+    print(number1 + number2)
